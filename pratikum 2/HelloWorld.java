b80ad7ec063e22fac840507ef2fef9c6ed8cffe9
Rifki Rizkia <rifkirizkia91@gmail.com>
2020-09-16 11:32:04 +0700

    HelloWorld.java

---
+++ pratikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Rifki!");
+		System.out.println("NIM:6706201139");
+		System.out.println("Kelas:D3IF-44-02");
+	}
+}
